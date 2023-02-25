e7b725c27ccb7dfc6d3ac21e6f6fb88e67418a57
Andrew Bolt <40577677+ambolt314@users.noreply.github.com>
2023-02-25 23:18:25 +0000

    Created inventory report class with basic report method

---
+++ SoftStocks/SoftStocksData/Report.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftStocksData
+{
+    internal class Report
+    {
+        private string Id;
+        private DateTime CreationTimestamp;
+
+        public Report(string id)
+        {
+            this.Id = id;
+            this.CreationTimestamp = DateTime.Now;
+
+        }
+
+        public object Generate(ReportFormat format)
+        {
+            return true;
+        }
+
+        public void Destroy()
+        {
+
+        }
+    }
+}
