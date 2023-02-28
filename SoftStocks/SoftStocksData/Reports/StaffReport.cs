46e969724765ad3eea76160c0b7c89c2757568f4
Andrew Bolt <40577677+ambolt314@users.noreply.github.com>
2023-02-28 17:04:32 +0000

    Formatted staff report PDF with basic information

--- SoftStocks/SoftStocksData/Reports/StaffReport.cs
+++ SoftStocks/SoftStocksData/Reports/StaffReport.cs
@@ -46,11 +46,18 @@ namespace SoftStocksData.Reports
                     XFont titleFont = new("Segoe UI", 24, XFontStyle.Bold);
                     XFont bodyFont = new("Segoe UI", 12, XFontStyle.Regular);
 
-                    gfx.DrawString($"Directory - {LastName}, {FirstName} ({Id})", titleFont, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
-                    gfx.DrawString($"Staff member report, created on {CreationTimestamp.Date}", bodyFont, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
-                    gfx.DrawString($"Id: {Id}\nTitle: {Title}\nFirst name: {FirstName}", bodyFont, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+                    gfx.DrawString($"Directory - {LastName}, {FirstName} ({Id})", titleFont, XBrushes.Black, new XRect(50, -350, page.Width, page.Height), XStringFormats.CenterLeft);
+                    gfx.DrawString($"Staff member listing for SoftStocks employee {FirstName} {LastName}", bodyFont, XBrushes.Black, new XRect(50, -300, page.Width, page.Height), XStringFormats.CenterLeft);
+                    gfx.DrawString($"Id: {Id}", bodyFont, XBrushes.Black, new XRect(50, -250, page.Width, page.Height), XStringFormats.CenterLeft);
+                    gfx.DrawString($"Title: {Title}", bodyFont, XBrushes.Black, new XRect(50, -225, page.Width, page.Height), XStringFormats.CenterLeft);
+                    gfx.DrawString($"First name: {FirstName}", bodyFont, XBrushes.Black, new XRect(50, -200, page.Width, page.Height), XStringFormats.CenterLeft);
+                    gfx.DrawString($"Last name: {LastName}", bodyFont, XBrushes.Black, new XRect(50, -175, page.Width, page.Height), XStringFormats.CenterLeft);
+                    gfx.DrawString($"Role: {Role}", bodyFont, XBrushes.Black, new XRect(50, -150, page.Width, page.Height), XStringFormats.CenterLeft);
+                    gfx.DrawString($"Date of birth: {DateOfBirth.Date:dd/MM/yyyy}", bodyFont, XBrushes.Black, new XRect(50, -125, page.Width, page.Height), XStringFormats.CenterLeft);
+                    gfx.DrawString($"Salary: £{Salary:N2}", bodyFont, XBrushes.Black, new XRect(50, -100, page.Width, page.Height), XStringFormats.CenterLeft);
 
-                    string fileName = "staff_report.pdf";
+
+                    string fileName = $"staff_report_{CreationTimestamp.Date.Year}{CreationTimestamp.Date.Month}{CreationTimestamp.Date.Day}{Id}.pdf";
                     pdfReport.Save(fileName);
 
                     return fileName;
