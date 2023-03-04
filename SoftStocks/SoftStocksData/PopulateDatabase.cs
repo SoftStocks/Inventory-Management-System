80dac12abfb7c58f80a6a947b32c7bcc2210c528
Andrew Bolt <40577677+ambolt314@users.noreply.github.com>
2023-03-04 19:16:53 +0000

    Added template for database population script

---
+++ SoftStocks/SoftStocksData/PopulateDatabase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftStocksData
+{
+    internal static class PopulateDatabase
+    {
+        public static void FromCSVFile(string fileName)
+        {
+            using(TextFieldParser parser = new TextFieldParser(fileName))
+            {
+                using (SoftStocksDBContext db = new SoftStocksDBContext())
+                {
+                    parser.TextFieldType = FieldType.Delimited;
+                    parser.SetDelimiters(",");
+
+                    while (!parser.EndOfData)
+                    {
+                        //Read the file
+                        //Create dbContext record based on the parsed data
+                        //Save changes
+                    }
+                }
+                
+            }
+        }
+    }
+}
