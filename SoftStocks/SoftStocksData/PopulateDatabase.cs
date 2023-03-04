0dec0b147147b495e029a3b89adf523a020a7155
Andrew Bolt <40577677+ambolt314@users.noreply.github.com>
2023-03-04 20:55:12 +0000

    fleshed out database population script design

--- SoftStocks/SoftStocksData/PopulateDatabase.cs
+++ SoftStocks/SoftStocksData/PopulateDatabase.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualBasic.FileIO;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,16 @@ namespace SoftStocksData
 
                     while (!parser.EndOfData)
                     {
+                        string[] fields = parser.ReadFields();
+
+                        // what are the fields stored in the database?
+                        // switch the table depending on which fields have been discovered
+
                         //Read the file
                         //Create dbContext record based on the parsed data
+                        db.Staff.Add(new Staff());
                         //Save changes
+                        db.SaveChanges();
                     }
                 }
                 
