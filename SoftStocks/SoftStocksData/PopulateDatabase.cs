942cf08aecd73d85999c9a8d147d13558ea31b77
Andrew Bolt <40577677+ambolt314@users.noreply.github.com>
2023-03-06 20:51:33 +0000

    Implemented population script. TODO - test changes

--- SoftStocks/SoftStocksData/PopulateDatabase.cs
+++ SoftStocks/SoftStocksData/PopulateDatabase.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -40,34 +41,84 @@ namespace SoftStocksData
                         // switch the table depending on which fields have been discovered
                         if (header.SequenceEqual(staffHeader))
                         {
-                            Console.WriteLine("STAFF");
+                            Staff newStaff = new Staff {
+                                Title = fields[0],
+                                FirstName = fields[1],
+                                LastName = fields[2],
+                                Role = fields[3],
+                                DateOfBirth = DateTime.Parse(fields[4], new CultureInfo("en-GB")),
+                                Salary = float.Parse(fields[5], CultureInfo.InvariantCulture.NumberFormat)
+                            };
+                            db.Staff.Add(newStaff);
                         }
                         else if (header.SequenceEqual(credentialsHeader))
                         {
-                            //db.Credentials.Add(new Credential { });
+                            Credential newCredential = new Credential
+                            {
+                                Username = fields[0],
+                                StaffId = int.Parse(fields[1]),
+                                Password = fields[2]
+                            };
+                            db.Credentials.Add(newCredential);
                         }
-                        else if(header.SequenceEqual(staffHeader))
+                        else if(header.SequenceEqual(keyboardHeader))
                         {
-                            //db.KeyboardRequests.Add(new KeyboardRequest { });
+                            Keyboard newKeyboard = new Keyboard
+                            {
+                                ModelNumber = int.Parse(fields[0]),
+                                SupplierId = int.Parse(fields[1]),
+                                Quantity = int.Parse(fields[2]),
+                                Description = fields[3],
+                                Price = (System.Data.SqlTypes.SqlMoney)float.Parse(fields[4])
+                            };
+                            db.Keyboards.Add(newKeyboard);
                         }
-                        else if (header.SequenceEqual(staffHeader))
+                        else if (header.SequenceEqual(supplierHeader))
                         {
-                            //db.Suppliers.Add(new Supplier { });
+                            Supplier newSupplier = new Supplier
+                            {
+                                Name = fields[0],
+                                ContactNumber = fields[1],
+                                PrimaryContact = fields[2],
+                                BusinessAddress = fields[3],
+                                NumberOfPurchases = int.Parse(fields[4])
+                            };
+                            db.Suppliers.Add(newSupplier);
                         }
-                        else if(header.SequenceEqual(staffHeader))
+                        else if(header.SequenceEqual(purchaseRequestHeader))
                         {
-                            //db.PurchaseRequests.Add(new PurchaseRequest { });
+                            PurchaseRequest newPurchaseRequest = new PurchaseRequest
+                            {
+                                KeyboardRequestId = int.Parse(fields[0]),
+                                Quantity = int.Parse(fields[1]),
+                                StaffId = int.Parse(fields[2]),
+                                Approved = bool.Parse(fields[3])
+                            };
+                            db.PurchaseRequests.Add(newPurchaseRequest);
                         }
-                        else if(header.SequenceEqual(staffHeader))
+                        else if(header.SequenceEqual(keyboardRequestHeader))
                         {
-                            //db.KeyboardRequests.Add(new KeyboardRequest { });
+                            KeyboardRequest newKeyboardRequest = new KeyboardRequest
+                            {
+                                ModelNumber = fields[0],
+                                PurchaseRequestId = int.Parse(fields[1])
+                            };
+                            db.KeyboardRequests.Add(newKeyboardRequest);
                         }
 
-                        // Save changes
                         db.SaveChanges();
                     }
                 }
                 
+            }
+        }
+
+        public static void ClearFrom(string table)
+        {
+            using (var context = new SoftStocksDBContext())
+            {
+                context.Database.ExecuteSqlCommand($"TRUNCATE TABLE {table}");
+                context.SaveChanges();
             }
         }
     }
