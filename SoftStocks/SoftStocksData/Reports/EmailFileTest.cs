ef316428eb81685a4f091c583e84640d396760f7
J-Codes17 <jackwallace2002@hotmail.co.uk>
2023-03-07 17:42:11 +0000

    Made Email Report File

---
+++ SoftStocks/SoftStocksData/Reports/EmailFileTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftStocksData.Reports
+{
+    internal class EmailFileTest
+    {
+
+        //This Is Just A Template For Now - 07-03-23 - Jack Wallace
+
+    }
+}
