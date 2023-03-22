c04eec73ae16336583001073ab86e9b977e96110
Andrew Bolt <40577677+ambolt314@users.noreply.github.com>
2023-03-22 17:39:55 +0000

    Modified access levels for helper methods

--- SoftStocks/SoftStocksData/PopulateDatabase.cs
+++ SoftStocks/SoftStocksData/PopulateDatabase.cs
@@ -1,18 +1,16 @@
 ﻿using Microsoft.VisualBasic.FileIO;
-using System;
-using System.Collections.Generic;
-using System.Data.Entity;
+using SoftStocksData.Entities.Keyboards;
+using SoftStocksData.Entities.Purchases;
+using SoftStocksData.Entities.StaffMember;
+using SoftStocksData.Entities.Supplier;
+using SoftStocksData.Keyboards;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web;
 
 namespace SoftStocksData
 {
     public static class PopulateDatabase
     {
-        public static void FromCSVFile(string fileName)
+        private static void FromCSVFile(string fileName)
         {
             using(TextFieldParser parser = new TextFieldParser(fileName))
             {
@@ -24,13 +22,13 @@ namespace SoftStocksData
                     string[] header = parser.ReadFields();
 
                     // database fields
-                    string[] staffHeader = { "title", "first_name", "last_name", "role", "date_of_birth", "salary" };
+                    string[] staffHeader = { "id", "title", "first_name", "last_name", "role", "date_of_birth", "salary" };
                     string[] credentialsHeader = { "username", "staff_id", "password" };
                     string[] keyboardHeader = { "model_number", "supplier_id", "quantity", "description", "price" };
-                    string[] supplierHeader = { "name", "contact_number", "primary_contact", "business_address", "number_of_purchases" };
-                    string[] purchaseRequestHeader = {"keyboard_requestid", "quantity", "staff_id", "approved" };
-                    string[] keyboardRequestHeader = { "model_number", "purchase_request_id" };
-
+                    string[] supplierHeader = { "id", "name", "contact_number", "primary_contact", "business_address", "number_of_purchases" };
+                    string[] purchaseRequestHeader = {"id", "keyboard_requestid", "quantity", "staff_id", "approved" };
+                    string[] keyboardRequestHeader = { "id", "model_number", "purchase_request_id" };
+                    string[] purchaseTransactionHeader = { "id", "purchase_requestid", "type" };
 
                     while (!parser.EndOfData)
                     {
@@ -42,12 +40,13 @@ namespace SoftStocksData
                         if (header.SequenceEqual(staffHeader))
                         {
                             Staff newStaff = new Staff {
-                                Title = fields[0],
-                                FirstName = fields[1],
-                                LastName = fields[2],
-                                Role = fields[3],
-                                DateOfBirth = DateTime.Parse(fields[4], new CultureInfo("en-GB")),
-                                Salary = float.Parse(fields[5], CultureInfo.InvariantCulture.NumberFormat)
+                                Id = int.Parse(fields[0]),
+                                Title = fields[1],
+                                FirstName = fields[2],
+                                LastName = fields[3],
+                                Role = fields[4],
+                                DateOfBirth = DateTime.Parse(fields[5], new CultureInfo("en-GB")),
+                                Salary = float.Parse(fields[6], CultureInfo.InvariantCulture.NumberFormat)
                             };
                             db.Staff.Add(newStaff);
                         }
@@ -77,11 +76,11 @@ namespace SoftStocksData
                         {
                             Supplier newSupplier = new Supplier
                             {
-                                Name = fields[0],
-                                ContactNumber = fields[1],
-                                PrimaryContact = fields[2],
-                                BusinessAddress = fields[3],
-                                NumberOfPurchases = int.Parse(fields[4])
+                                Id = int.Parse(fields[0]),
+                                Name = fields[1],
+                                ContactNumber = fields[2],
+                                PrimaryContact = fields[3],
+                                BusinessAddress = fields[4]
                             };
                             db.Suppliers.Add(newSupplier);
                         }
@@ -89,10 +88,10 @@ namespace SoftStocksData
                         {
                             PurchaseRequest newPurchaseRequest = new PurchaseRequest
                             {
-                                KeyboardRequestId = int.Parse(fields[0]),
-                                Quantity = int.Parse(fields[1]),
-                                StaffId = int.Parse(fields[2]),
-                                Approved = bool.Parse(fields[3])
+                                Id = int.Parse(fields[0]),
+                                KeyboardRequestId = int.Parse(fields[1]),
+                                Quantity = int.Parse(fields[2]),
+                                StaffId = int.Parse(fields[3])
                             };
                             db.PurchaseRequests.Add(newPurchaseRequest);
                         }
@@ -100,10 +99,21 @@ namespace SoftStocksData
                         {
                             KeyboardRequest newKeyboardRequest = new KeyboardRequest
                             {
-                                ModelNumber = fields[0],
-                                PurchaseRequestId = int.Parse(fields[1])
+                                Id = int.Parse(fields[0]),
+                                ModelNumber = int.Parse(fields[1]),
+                                PurchaseRequestId = int.Parse(fields[2])
                             };
                             db.KeyboardRequests.Add(newKeyboardRequest);
+                        }
+                        else if(header.SequenceEqual(purchaseTransactionHeader))
+                        {
+                            PurchaseTransaction newPurchaseTransaction = new PurchaseTransaction
+                            {
+                                Id = int.Parse(fields[0]),
+                                PurchaseRequestId = int.Parse(fields[1]),
+                                Type = fields[2]
+                            };
+                            db.PurchaseTransactions.Add(newPurchaseTransaction);
                         }
 
                         db.SaveChanges();
@@ -113,8 +123,11 @@ namespace SoftStocksData
             }
         }
 
-        public static void Clear()
+        private static void Clear()
         {
+            
+
+            
             using (var context = new SoftStocksDBContext())
             {
 
@@ -124,7 +137,11 @@ namespace SoftStocksData
                 context.Keyboards.RemoveRange(context.Keyboards.ToList());
                 context.PurchaseRequests.RemoveRange(context.PurchaseRequests.ToList());
                 context.KeyboardRequests.RemoveRange(context.KeyboardRequests.ToList());
+
+                //context.Database.ExecuteSqlCommand("DBCC CHECKIDENT('Credentials', RESEED, 1)");
                 
+
+
                 context.SaveChanges();
             }
         }
