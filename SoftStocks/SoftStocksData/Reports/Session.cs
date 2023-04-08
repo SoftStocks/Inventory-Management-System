f020e3118d2505c79b2e542943e65f13b6266095
Andrew Bolt <40577677+ambolt314@users.noreply.github.com>
2023-04-08 15:16:56 +0100

    Added singleton for session information, implemented report storage

---
+++ SoftStocks/SoftStocksData/Reports/Session.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftStocksData.Reports
+{
+	public sealed class Session
+	{
+		private static Session instance;
+
+		//Session data to be persisted
+		private static List<Report> reports = new List<Report>();
+		private Session() {
+			reports = new List<Report>();
+		}
+
+		public static Session Instance
+		{
+			get {
+				if (instance == null)
+				{
+					instance = new Session();
+				}
+				return instance;
+			}
+		}
+
+		public static void AddReport(Report report)
+		{
+			reports.Add(report);
+		}
+
+		public static void RemoveReportById(int Id)
+		{
+			reports.RemoveAll(r => r.Id == Id);
+		}
+	}
+}
