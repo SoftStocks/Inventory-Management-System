9bedd220413f97eb0d5361cc0e551c2635880794
Andrew Bolt <40577677+ambolt314@users.noreply.github.com>
2023-04-17 22:14:18 +0100

    Resolved develop -> feature merge conflict

--- SoftStocks/SoftStocksData/PopulateDatabase.cs
+++ SoftStocks/SoftStocksData/PopulateDatabase.cs
@@ -69,7 +69,7 @@ namespace SoftStocksData
                                 SupplierId = int.Parse(fields[1]),
                                 Quantity = int.Parse(fields[2]),
                                 Description = fields[3],
-                                Price = (System.Data.SqlTypes.SqlMoney)float.Parse(fields[4])
+                                Price = float.Parse(fields[4])
                             };
                             db.Keyboards.Add(newKeyboard);
                         }
